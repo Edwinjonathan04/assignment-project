90103b04e3e654300355cdb4d9e456a734adf167
abbaishaya899 <150626336+abbaishaya899@users.noreply.github.com>
2024-06-06 03:31:45 -0700

    Update my_group_work.py
    i modified your code

--- my_group_work.py
+++ my_group_work.py
@@ -17,3 +17,20 @@ class Account:
     def get_balance(self):
         return self._balance
 
+
+class SavingsAccount(Account):
+    def __init__(self, balance=0):
+        super().__init__(balance)
+
+    def deposit(self, amount):
+        if amount <= 700000:
+            super().deposit(amount)
+            self._balance *= 1.005  # 0.5% interest
+        else:
+            print("Deposit limit exceeded for Savings Account!")
+
+
+class CurrentAccount(Account):
+    def __init__(self, balance=0):
+        super().__init__(balance)
+
